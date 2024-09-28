5cbf684cb8a37ff16f8ff5306b9cbe0dff194dff
Mark Ryk <marco.look@outlook.com>
2024-09-28 15:55:23 -0300

    configurando bd, associando dados, renderização estática e dinâmica, streaming com arq. loading e Suspense, implementado Busca e Paginação

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,17 +9,17 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-export async function fetchRevenue() {
+export async function fetchRevenue() { {/* 1 */}
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -28,7 +28,7 @@ export async function fetchRevenue() {
   }
 }
 
-export async function fetchLatestInvoices() {
+export async function fetchLatestInvoices() { {/* 2 */}
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
